2cdb3d6c86bd72a744eaa4939b4531e5ecd66eff
kohnik <kohnik.samp@gmail.com>
2021-04-12 19:00:52 +0300

    auth2

--- src/app/core/guards/guardAuth/auth-guard.guard.ts
+++ src/app/core/guards/guardAuth/auth-guard.guard.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
 import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
 import { Observable } from 'rxjs';
-import {FirebaseService} from '../services/firebase.service';
+import {FirebaseService} from './core/services/firebase.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthHomeGuardGuard implements CanActivate {
-  constructor(public firebase: FirebaseService){}
+export class AuthGuard implements CanActivate {
+  constructor(public firebase: FirebaseService, private router: Router){}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return localStorage.getItem('user') !== null;
+    if (localStorage.getItem('user') !== null)
+    {
+      this.router.navigate(['home']);
+    }
+    return localStorage.getItem('user') === null;
   }
 }
+
---
+++ src/app/core/guards/guardHome/auth-home-guard.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthHomeGuardGuard implements CanActivate {
+  constructor() {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return localStorage.getItem('user') !== null;
+  }
+}
